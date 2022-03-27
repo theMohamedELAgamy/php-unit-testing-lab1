d38ecddcf0f090a8f9788cb7f19c35176ab5a91a
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-03-27 17:11:59 +0200

    updated

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 
 
-     // my app includes
+     
  include('function.php');
-     //include('./app/lib.php');
-//	require dirname(__DIR__).'/home/mohamed/testing_lab1/function.php'
+  
 
 
 
